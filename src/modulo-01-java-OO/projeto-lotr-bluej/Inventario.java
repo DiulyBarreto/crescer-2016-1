21ee20f5b757b880413b4fe458c32a62114cbad1
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-04-07 17:32:35 -0300

    feat(Classes): adicionar classes Inventario e Item

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Inventario{
+    
+    private ArrayList<Item> lista = new ArrayList<>();
+    
+    public void adicionarItem(Item item){
+        lista.add(item);
+    }
+    
+    public void removerItem(Item item){
+        lista.remove(item);
+    }
+}
