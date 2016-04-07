26b098dc8651477e06c6aef8041b67f3388ed04c
Diuly Barreto <Diuly Barreto>
2016-04-07 11:03:36 -0300

    Lista 4

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEra.java
@@ -0,0 +1,28 @@
+public class DataTerceiraEra{
+    private int dia;
+    private int mes;
+    private int ano;
+    
+    public DataTerceiraEra(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
+    public int getDia(){
+        return dia;
+    }
+    
+    public int getMes(){
+        return mes;
+    }
+    
+    public int getAno(){
+        return ano;
+    }
+    
+    public boolean ehBissexto(){
+        boolean bissexto = ano % 4 == 0 && ano % 100 != 0 || (ano % 100 == 0 && ano % 400 == 0);
+        return bissexto; 
+    }
+}
