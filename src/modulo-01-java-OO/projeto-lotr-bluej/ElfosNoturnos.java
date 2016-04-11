d4dd8cef3930a22ce2d1d282308c030bddb40352
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-04-11 17:44:38 -0300

    feat(ElfosNoturnos): adicionar classe de elfos especializada.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfosNoturnos.java
@@ -0,0 +1,11 @@
+public class ElfosNoturnos extends Elfo {
+    
+    public ElfosNoturnos(String nome){
+        super(nome);
+    }
+    
+    public ElfosNoturnos(String nome, int flechas){
+        super(nome, flechas);
+    }
+    
+}
