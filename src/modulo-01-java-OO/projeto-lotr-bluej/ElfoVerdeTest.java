599907634bbbe03a1eb2e4e5dc9beb3067b3685a
Diuly Barreto <Diuly Barreto>
2016-04-12 09:21:20 -0300

    test(ElfoVerde): adicionar cenários para adicionar itens com diferentes descrições.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
@@ -8,9 +8,11 @@ public class ElfoVerdeTest
     @Test
     public void ganharDobroDeExperienciaComparadoAoElfoNormal(){
         ElfoVerde elfoVerde = new ElfoVerde("Elfo Verde");
-        int esperado = 2;
+        Dwarf d = new Dwarf("Dwarf");
+        int esperado = 4;
         
-        elfoVerde.atirarFlecha();
+        elfoVerde.atirarFlechaDwarf(d);
+        elfoVerde.atirarFlechaDwarf(d);
         
         assertEquals(esperado, elfoVerde.getExperiencia());
     }
@@ -22,6 +24,46 @@ public class ElfoVerdeTest
          elfoVerde.adicionarItem(item);
          
          assertEquals(item, elfoVerde.getInventario().getLista().get(0));
+    }
+    
+    @Test
+    public void adicionarItensComDescricaoDiferenteDaPermitida(){
+        ElfoVerde elfoVerde = new ElfoVerde("Elfo Verde");
+        Item item = new Item(1, "Espada de aço velho");
+        int tamanhoEsperado = 0;
+        
+        elfoVerde.adicionarItem(item);
+         
+        assertEquals(tamanhoEsperado, elfoVerde.getInventario().getLista().size()); 
+    }
+    
+    @Test
+    public void adicionarItemComDescricaoVazia(){
+        ElfoVerde elfoVerde = new ElfoVerde("Elfo Verde");
+        Item item = new Item(1, "");
+        elfoVerde.adicionarItem(item);
+        int tamanhoEsperado = 0;
+         
+        assertEquals(tamanhoEsperado, elfoVerde.getInventario().getLista().size());
+    }
+    
+    @Test
+    public void adicionarItemComDescricaoNula(){
+        ElfoVerde elfoVerde = new ElfoVerde("Elfo Verde");
+        Item item = new Item(1, null);
+        elfoVerde.adicionarItem(item);
+        int tamanhoEsperado = 0;
+         
+        assertEquals(tamanhoEsperado, elfoVerde.getInventario().getLista().size());
+    }
+    
+    @Test
+    public void adicionarItemComDescricaoIgnorandoLetrasMaiusculas(){
+        ElfoVerde elfoVerde = new ElfoVerde("Elfo Verde");
+        Item item = new Item(1, "Espada De Aço Valiriano");
+        elfoVerde.adicionarItem(item);
+         
+        assertEquals(item, elfoVerde.getInventario().getLista().get(0));
     }
     
     @Test
