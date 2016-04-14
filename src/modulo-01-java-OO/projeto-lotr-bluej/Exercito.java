00aa9f3f4be962ee9ad060bcf422ff00319b902c
Diuly Barreto <Diuly Barreto>
2016-04-14 12:42:18 -0300

    commit temporário (COM ERROS) apenas para que o código seja acessado em outro notebook.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Exercito.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Exercito.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Exercito {
     
     private HashMap<String, Elfo> exercitoElfos = new HashMap<>();
+    private HashMap<Status, ArrayList<Elfo>> exercitoAgrupado = new HashMap<>();
     
     public void alistarElfo(Elfo elfo) {
         if(elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno)
@@ -16,8 +17,30 @@ public class Exercito {
         return null;
     }
     
-    public HashMap<String, Elfo> getAlistados(){
+    public HashMap<String, Elfo> getAlistados() {
         return exercitoElfos;
+    }
+    
+    public void agruparPorStatus() {
+        ArrayList vivo = new ArrayList();
+        ArrayList morto = new ArrayList();
+        
+        for(Elfo elfo : exercitoElfos.values()){
+            //exercitoAgrupado.get(elfo.getStatus()).add(elfo);
+            
+            if(elfo.getStatus() == Status.VIVO)
+                vivo.add(elfo);
+            else
+                morto.add(elfo);
+             
+            exercitoAgrupado.put(Status.VIVO, vivo);
+            exercitoAgrupado.put(Status.MORTO, morto);
+        }
+        
+    }
+    
+    public HashMap<Status, ArrayList<Elfo>> getExercitoAgrupado(){
+        return exercitoAgrupado;
     }
 }
 
--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoTest.java
@@ -59,4 +59,42 @@ public class ExercitoTest {
         assertNull(exercito.buscarNome("Diuly Any"));
     }
     
+    @Test
+    public void encontrarUmElfoNoturnoPeloNome(){
+        ElfoNoturno elfo1 = new ElfoNoturno("Diuly");
+        Exercito exercito = new Exercito();
+        exercito.alistarElfo(elfo1);
+        
+        assertEquals(elfo1, exercito.buscarNome("Diuly"));
+    }
+    
+    @Test
+    public void agruparElfosVivosEMortos(){
+        Elfo elfoMorto1 = criarElfoEDepoisMataLo("Renan");
+        Elfo elfoMorto2 = criarElfoEDepoisMataLo("Pedro");
+        ElfoNoturno elfo1 = new ElfoNoturno("Diuly");
+        ElfoVerde elfo2 = new ElfoVerde("Gui");
+        Exercito exercito = new Exercito();
+        exercito.alistarElfo(elfoMorto1);
+        exercito.alistarElfo(elfoMorto2);
+        exercito.alistarElfo(elfo1);
+        exercito.alistarElfo(elfo2);
+        
+        exercito.agruparPorStatus();
+        
+        assertTrue(exercito.getExercitoAgrupado().get(Status.VIVO).contains(elfo1));
+        assertTrue(exercito.getExercitoAgrupado().get(Status.VIVO).contains(elfo2));
+        assertTrue(exercito.getExercitoAgrupado().get(Status.MORTO).contains(elfoMorto1));
+    }
+    
+    
+    private Elfo criarElfoEDepoisMataLo(String nome){
+        Elfo elfo = new ElfoNoturno(nome);
+        
+        for(int i = 0; i < 90; i++){
+            elfo.atirarFlechaDwarf(new Dwarf("Balin"));
+        }
+        
+        return elfo;
+    }
 }
