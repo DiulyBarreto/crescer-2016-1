1b53b080cbbc5f28625290776f98abf6bfde142e
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-04-14 17:27:06 -0300

    feat(DicionarioSindarin): acrescentar classes para traduzir palavras do Sindarin para o Português ou Inglês.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/SindarinParaIngles.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SindarinParaIngles implements TradutorSindarin {
+    
+    private HashMap<String, String> dicionario = new HashMap<String, String>()
+    {
+        {
+            put("naur", "fire");
+        }
+    };
+    
+    public String traduzir(String palavraEmSindarin) {
+        return dicionario.get(palavraEmSindarin);
+    }
+    
+    // não está na interface
+    public void limpar(){
+        dicionario.clear();
+    }
+}
