53ce433dd87f30a4e90d5c9f347c4837a256f111
Diuly Barreto <Diuly Barreto>
2016-04-17 20:56:34 -0300

    feat(EstrategiaAtaqueIntercalado): adicionar classe que implementa estratégia de ataque intercalado.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaAtaqueIntercalado.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class EstrategiaAtaqueIntercalado implements EstrategiasDeCombate {
+    
+    ArrayList<Elfo> ordemDoUltimoAtaque = new ArrayList<>();
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemDoUltimoAtaque;
+    }
+    
+    public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarf> dwarves) {
+        removerMortos(pelotao);
+        
+        if(ehMeioAMeio(pelotao)) {
+            ordenarPelotao(pelotao);
+            
+            for(int i = 0; i < pelotao.size(); i++) {
+                for(int j = 0; j < dwarves.size(); j++) {
+                        pelotao.get(i).atirarFlechaDwarf(dwarves.get(j));
+                }
+                ordemDoUltimoAtaque.add(pelotao.get(i));
+            }
+        }
+            
+    }
+        
+    private void ordenarPelotao(ArrayList<Elfo> pelotao) {
+        for(int i = 0; i < pelotao.size() - 1; i++) {
+            if(pelotao.get(i).getClass().equals(pelotao.get(i + 1).getClass()))
+                for(int j = i+1; j < pelotao.size(); j++) {
+                    if(!(pelotao.get(i).getClass().equals(pelotao.get(j).getClass()))) {
+                        Elfo aux = pelotao.get(i + 1);
+                        pelotao.set(i + 1, pelotao.get(j));
+                        pelotao.set(j, aux);
+                    }
+                        
+                }
+        }
+    }
+    
+    private boolean ehMeioAMeio(ArrayList<Elfo> pelotao) {
+        int contElfosVerdes = 0;
+        int contElfosNoturnos = 0;
+        
+        for(int i = 0; i < pelotao.size(); i++) {
+            if(pelotao.get(i) instanceof ElfoVerde)
+                contElfosVerdes++;
+            else
+                contElfosNoturnos++;
+        }
+        
+        return contElfosVerdes == contElfosNoturnos ? true : false;
+    }
+    
+    private void removerMortos(ArrayList<Elfo> pelotao) {
+        for(int i = 0; i < pelotao.size(); i++) {
+            if(pelotao.get(i).getStatus().equals(Status.MORTO))
+                pelotao.remove(i);
+        }
+    }
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaAtaqueIntercaladoTest.java
@@ -0,0 +1,167 @@
+
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EstrategiaAtaqueIntercaladoTest
+{
+    @Test
+    public void atacarComPelotaoMeioAMeio() {
+        Elfo elfo1 = new ElfoVerde("Andy");
+        Elfo elfo2 = new ElfoVerde("Ave");
+        Elfo elfo3 = new ElfoNoturno("Diuly");
+        Elfo elfo4 = new ElfoNoturno("Adão");
+        ArrayList<Elfo> pelotao = new ArrayList<>();
+        pelotao.add(elfo1);
+        pelotao.add(elfo2);
+        pelotao.add(elfo3);
+        pelotao.add(elfo4);
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaAtaqueIntercalado();
+        e.atacar(pelotao, dwarves);
+        
+        // certificar que todos atacaram todos os dwarves
+        assertEquals(39, pelotao.get(0).getFlechas());
+        assertEquals(39, pelotao.get(1).getFlechas());
+        assertEquals(39, pelotao.get(2).getFlechas());
+        assertEquals(39, pelotao.get(3).getFlechas());
+        
+    }
+    
+    @Test
+    public void atacarComPelotaoNaoProporcional() {
+        Elfo elfo1 = new ElfoVerde("Andy");
+        Elfo elfo2 = new ElfoVerde("Ave");
+        Elfo elfo3 = new ElfoVerde("Andy");
+        Elfo elfo4 = new ElfoNoturno("Diuly");
+        Elfo elfo5 = new ElfoNoturno("Adão");
+        ArrayList<Elfo> pelotao = new ArrayList<>();
+        pelotao.add(elfo1);
+        pelotao.add(elfo2);
+        pelotao.add(elfo3);
+        pelotao.add(elfo4);
+        pelotao.add(elfo5);
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaAtaqueIntercalado();
+        e.atacar(pelotao, dwarves);
+        
+        // certificar que todos atacaram todos os dwarves
+        assertEquals(42, pelotao.get(0).getFlechas());
+        assertEquals(42, pelotao.get(1).getFlechas());
+        assertEquals(42, pelotao.get(2).getFlechas());
+        assertEquals(42, pelotao.get(3).getFlechas());
+        assertEquals(42, pelotao.get(4).getFlechas());
+    }
+    
+    @Test
+    public void atacarComDoisMortosNoPelotao() {
+        Elfo elfo1 = new ElfoVerde("Andy");
+        Elfo elfo2 = new ElfoVerde("Ave");
+        Elfo elfo3 = new ElfoNoturno("Diuly");
+        Elfo elfo4 = new ElfoNoturno("Adão");
+        Elfo elfo5 = criarElfoEDepoisMataLo("Pedro");
+        Elfo elfo6 = criarElfoEDepoisMataLo("Lucas");
+        ArrayList<Elfo> pelotao = new ArrayList<>();
+        pelotao.add(elfo1);
+        pelotao.add(elfo2);
+        pelotao.add(elfo3);
+        pelotao.add(elfo4);
+        pelotao.add(elfo5);
+        pelotao.add(elfo6);
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaAtaqueIntercalado();
+        e.atacar(pelotao, dwarves);
+        
+        assertFalse(e.getOrdemDoUltimoAtaque().contains(elfo5));
+        assertFalse(e.getOrdemDoUltimoAtaque().contains(elfo6));
+        
+    }
+    
+    private Elfo criarElfoEDepoisMataLo(String nome){
+        Elfo elfo = new ElfoNoturno(nome);
+        for(int i = 0; i < 90; i++){
+            elfo.atirarFlechaDwarf(new Dwarf("Adão"));
+        }
+        
+        return elfo;
+    }
+    
+    @Test
+    public void ataqueIntercaladoComecandoPorElfoNoturno() {
+        Elfo elfo1 = new ElfoNoturno("Diuly");
+        Elfo elfo2 = new ElfoNoturno("Adão");
+        Elfo elfo3 = new ElfoVerde("Andy");
+        Elfo elfo4 = new ElfoVerde("Ave");
+        ArrayList<Elfo> pelotao = new ArrayList<>();
+        pelotao.add(elfo1);
+        pelotao.add(elfo2);
+        pelotao.add(elfo3);
+        pelotao.add(elfo4);
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaAtaqueIntercalado();
+        e.atacar(pelotao, dwarves);
+        
+        assertTrue(e.getOrdemDoUltimoAtaque().get(0) instanceof ElfoNoturno);
+        assertTrue(e.getOrdemDoUltimoAtaque().get(1) instanceof ElfoVerde);
+        assertTrue(e.getOrdemDoUltimoAtaque().get(2) instanceof ElfoNoturno);
+        assertTrue(e.getOrdemDoUltimoAtaque().get(3) instanceof ElfoVerde);
+    }
+    
+    @Test
+    public void ataqueIntercaladoComecandoPorElfoVerde() {
+        Elfo elfo1 = new ElfoVerde("Andy");
+        Elfo elfo2 = new ElfoVerde("Ave");
+        Elfo elfo3 = new ElfoNoturno("Diuly");
+        Elfo elfo4 = new ElfoNoturno("Adão");
+        ArrayList<Elfo> pelotao = new ArrayList<>();
+        pelotao.add(elfo1);
+        pelotao.add(elfo2);
+        pelotao.add(elfo3);
+        pelotao.add(elfo4);
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaAtaqueIntercalado();
+        e.atacar(pelotao, dwarves);
+        
+        assertTrue(e.getOrdemDoUltimoAtaque().get(0) instanceof ElfoVerde);
+        assertTrue(e.getOrdemDoUltimoAtaque().get(1) instanceof ElfoNoturno);
+        assertTrue(e.getOrdemDoUltimoAtaque().get(2) instanceof ElfoVerde);
+        assertTrue(e.getOrdemDoUltimoAtaque().get(3) instanceof ElfoNoturno);
+    }
+}
