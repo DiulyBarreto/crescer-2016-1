175e512e1331e32d58625625da04e9b0a0a40646
Diuly Barreto <Diuly Barreto>
2016-04-17 22:53:17 -0300

    test(EstrategiaNoturnosPorUltimo): adicionar testes.

--- src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaNoturnosPorUltimoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaNoturnosPorUltimoTest.java
@@ -95,4 +95,114 @@ public class EstrategiaNoturnosPorUltimoTest {
         
         return elfo;
     }
+    
+    @Test
+    public void atacarComExercitoVazio() {
+        EstrategiasDeCombate e = new EstrategiaNoturnosPorUltimo();
+        ArrayList<Elfo> exercito = new ArrayList<>();
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        e.atacar(exercito, dwarves);
+        
+        assertTrue(e.getOrdemDoUltimoAtaque().isEmpty());
+    }
+    
+    @Test
+    public void atacarComExercitoSoDeNoturnos() {
+        ElfoNoturno elfo1 = new ElfoNoturno("Diuly");
+        ElfoNoturno elfo2 = new ElfoNoturno("Adão");
+        ElfoNoturno elfo3 = new ElfoNoturno("João");
+        EstrategiasDeCombate e = new EstrategiaNoturnosPorUltimo();
+        ArrayList<Elfo> exercito = new ArrayList<>();
+        exercito.add(elfo1); 
+        exercito.add(elfo2); 
+        exercito.add(elfo3); 
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        e.atacar(exercito, dwarves);
+        
+        assertEquals(elfo1, e.getOrdemDoUltimoAtaque().get(0));
+        assertEquals(elfo2, e.getOrdemDoUltimoAtaque().get(1));
+        assertEquals(elfo3, e.getOrdemDoUltimoAtaque().get(2));
+    }
+    
+    @Test
+    public void atacarComExercitoSoDeVerdes() {
+        Elfo elfo1 = new ElfoVerde("Andy");
+        Elfo elfo2 = new ElfoVerde("Ave");
+        Elfo elfo3 = new ElfoVerde("Diuly");
+        Elfo elfo4 = new ElfoVerde("Adão");
+        ArrayList<Elfo> exercito = new ArrayList<>();
+        exercito.add(elfo1); 
+        exercito.add(elfo2); 
+        exercito.add(elfo3); 
+        exercito.add(elfo4); 
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaNoturnosPorUltimo();
+        e.atacar(exercito, dwarves);
+        
+        assertEquals(elfo1, e.getOrdemDoUltimoAtaque().get(0));
+        assertEquals(elfo2, e.getOrdemDoUltimoAtaque().get(1));
+        assertEquals(elfo3, e.getOrdemDoUltimoAtaque().get(2));
+        assertEquals(elfo4, e.getOrdemDoUltimoAtaque().get(3));
+    }
+    
+    @Test
+    public void atacarComApenasUmElfoVerde() {
+        Elfo elfo1 = new ElfoVerde("Andy");
+        ArrayList<Elfo> exercito = new ArrayList<>();
+        exercito.add(elfo1);
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaNoturnosPorUltimo();
+        e.atacar(exercito, dwarves);
+        
+        assertEquals(elfo1, e.getOrdemDoUltimoAtaque().get(0));
+        assertEquals(1, e.getOrdemDoUltimoAtaque().size());
+    }
+    
+    @Test
+    public void atacarComApenasUmElfoNoturno() {
+        ElfoNoturno elfo1 = new ElfoNoturno("Diuly");
+        ArrayList<Elfo> exercito = new ArrayList<>();
+        exercito.add(elfo1);
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        Dwarf d3 = new Dwarf("D3");
+        dwarves.add(d1);
+        dwarves.add(d2);
+        dwarves.add(d3);
+        
+        EstrategiasDeCombate e = new EstrategiaNoturnosPorUltimo();
+        e.atacar(exercito, dwarves);
+        
+        assertEquals(elfo1, e.getOrdemDoUltimoAtaque().get(0));
+        assertEquals(1, e.getOrdemDoUltimoAtaque().size());
+    }
 }
