1e313ac6e0a5d1ff703f450487344588c155a1ff
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-05-11 13:57:12 -0300

    feat(ExercícioMVC): adicionar exercício Street Fighter MVC.

---
+++ src/modulo-05-dotnet/MVC/MVC/Controllers/StreetFighterController.cs
@@ -0,0 +1,36 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class StreetFighterController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+
+        public ActionResult SobreMim()
+        {
+            var pessoa = new SobreMimModel()
+            {
+                Nome = "Diuly Barreto",
+                Idade = 20,
+                Curso = "Analise e Desenvolvimento de Sistemas",
+                Facebook = "https://www.facebook.com/diuly.barreto",
+                SrcImagem = "../Content/Imagens/sobre_mim.jpg"
+            };
+
+            return View(pessoa);
+        }
+    }
+}
---
+++ src/modulo-05-dotnet/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Curso { get; set; }
+        public string Facebook { get; set; }
+        public string SrcImagem { get; set; }
+
+    }
+}
