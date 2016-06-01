d5dcbe4995c42241b85659c55b68f7162cab84f2
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-06-01 13:53:49 -0300

    feat(ExercíciosSQL): adicionar exercícios de 1 a 6.

---
+++ src/modulo-07-BDII/Lista1.sql
@@ -0,0 +1,26 @@
+-- Exercício 1
+select c.IDCliente, c.Nome, d.Nome as NomeCidade, d.UF
+FROM Cliente c INNER JOIN Cidade d on c.IDCidade = d.IDCIDADE
+
+-- Exercício 2
+SELECT COUNT(*) from Pedido Where DATAPEDIDO BETWEEN TO_DATE('01/05/2016', 'DD/MM/RRRR') and TO_DATE('31/05/2016', 'DD/MM/RRRR')+.99999;
+
+-- Exercício 3
+select * from Cliente Where UPPER(NOME) like UPPER('%LTDA%') or UPPER(RAZAOSOCIAL) like UPPER('%Ltda%');
+
+-- Exercício 4
+-- Nome: Galocha Maragato Preço de custo: 35.67 Preço de venda: 77.95 Situação: A 
+CREATE SEQUENCE SQPRODUTO START WITH 8001 INCREMENT BY 1;
+INSERT INTO PRODUTO (IDPRODUTO, Nome, PRECOCUSTO, PRECOVENDA, SITUACAO)
+VALUES (SQPRODUTO.NEXTVAL, 'Galocha Maragato', 35.67, 77.95, 'A');
+
+-- Exercício 5
+select p.IDPRODUTO, p.NOME, p.DATACADASTRO, p.PRECOCUSTO, p.PRECOVENDA, p.SITUACAO 
+from produto p left join pedidoitem i on i.idproduto = p.idproduto Where i.idproduto is null;
+
+-- Exercício 6
+select * from pedido where IDCLIENTE = :pIDCLIENTE;
+
+
+
+
