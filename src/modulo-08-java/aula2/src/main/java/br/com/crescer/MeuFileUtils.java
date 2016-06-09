b8f9e4e33f1a5a98e683988b432fcafa16264b3d
Diuly Barreto <Diuly Barreto>
2016-06-09 11:30:38 -0300

    feat(MeuFileUtils): adicionar métodos para mk, rm, ls, mv.

--- src/modulo-08-java/aula2/src/main/java/br/com/crescer/MeuFileUtils.java
+++ src/modulo-08-java/aula2/src/main/java/br/com/crescer/MeuFileUtils.java
@@ -7,6 +7,10 @@ package br.com.crescer;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -22,7 +26,9 @@ public class MeuFileUtils {
             
         } catch (IOException e) {
             e.printStackTrace();
-        }   
+        } catch (NullPointerException e) {
+            System.out.println("Erro");
+        }
     }
     
     public void delete(String nome) {
@@ -33,10 +39,60 @@ public class MeuFileUtils {
             file.delete();
     }
     
+    public void getPath(String nome) {
+        File file = new File(nome);
+        String[] lista = null;
+        
+        if(file.isDirectory())
+            lista = file.list();
+        else {
+            lista = new String[1];
+            lista[0] = file.getAbsolutePath();
+        }
+        
+        for (String cont : lista) {
+            System.out.println(cont);
+        }
+    }
+    
+    public void moveFile(String nome, String novoNome) {
+        File file = new File(nome);
+        File dest = new File(novoNome);
+        if(file.isDirectory()) {
+            System.out.println("Arquivo inválido");
+        } else {
+            try {
+                // TODO - ver maneira de mover, sem precisar passar o caminho completo.
+                // http://stackoverflow.com/questions/1000183/reliable-file-renameto-alternative-on-windows
+                Files.move(file.toPath(), dest.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+            
+            } catch (IOException e) {
+                System.out.println("Erro ao acessar arquivo");
+            }
+            
+        }
+    }
+    
     public static void main(String[] args) {
-        MeuFileUtils file = new MeuFileUtils();
-        file.create("aula3\\aula.txt");
-        file.delete("aula3\\aula.txt");
+        String op = null;
+        MeuFileUtils utils = new MeuFileUtils();
+        do {
+            System.out.println("C:\\Users\\Name>");
+            Scanner scanner = new Scanner(System.in);
+            op = scanner.nextLine();
+            String nome = scanner.nextLine();
+            
+            if (op.equalsIgnoreCase("mk")) 
+                utils.create(nome);
+            else if (op.equalsIgnoreCase("rm")) 
+                utils.delete(nome);
+            else if (op.equalsIgnoreCase("ls")) 
+                utils.getPath(nome);
+            else if (op.equalsIgnoreCase("mv")) {
+                String novoNome = scanner.nextLine();
+                utils.moveFile(nome, novoNome);
+            }
+        } while (!op.equalsIgnoreCase("tchau"));
     }
     
 }
