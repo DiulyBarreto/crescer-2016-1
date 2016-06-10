d016e266379bf261bc6841598dac6ada2b729708
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-06-10 13:00:42 -0300

    feat(MeuSQLUtils): adicionar método que retorna nome das linhas e colunas.

--- src/modulo-08-java/aula3/src/main/java/br/com/crescer/MeuSQLUtils.java
+++ src/modulo-08-java/aula3/src/main/java/br/com/crescer/MeuSQLUtils.java
@@ -11,8 +11,11 @@ import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -56,8 +59,35 @@ public class MeuSQLUtils {
         return path.endsWith(".sql");
     }
     
+    public List<String> executaQuery(String query) {
+        List<String> lista = new ArrayList<>();
+        try (final Connection connection = ConnectionUtils.getConnection()) {
+            try (final Statement statement = connection.createStatement()) {
+                ResultSet rs = statement.executeQuery(query);
+                ResultSetMetaData rsmd = rs.getMetaData();
+                lista.add(rsmd.getColumnName(1) + " " + rsmd.getColumnName(2));
+                while (rs.next()) {
+                    lista.add(rs.getLong("ID_PESSOA") + " " + rs.getString("NM_PESSOA"));
+                }
+                rs.close();
+            } catch (final SQLException e) {
+                System.err.format("SQLException: %s", e);
+            } 
+            
+        }catch (SQLException e) {
+                System.err.format("SQLException: %s", e);
+        }
+        
+        return lista;
+    }
+    
     public static void main(String[] args) {
         MeuSQLUtils utils = new MeuSQLUtils();
         utils.executaComandosDeArquivo("C:\\Users\\diuly.barreto\\Documents\\crescer-2016-1\\src\\modulo-08-java\\aula3\\comando.sql");
+//        List<String> lista = new ArrayList();
+//        lista = utils.executaQuery("select * from Pessoa");
+//        for(int i = 0 ; i < lista.size(); i++) {
+//            System.out.println(lista.get(i));
+//        }
     }
 }
