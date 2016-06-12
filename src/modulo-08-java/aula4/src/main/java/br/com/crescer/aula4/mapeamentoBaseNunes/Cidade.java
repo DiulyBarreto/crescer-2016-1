e04f96894cc8a60ea0ccdc855cf14890746c1c95
Diuly Barreto <Diuly Barreto>
2016-06-12 14:59:32 -0300

    feat(Cidade): adicionar classe para mapear tabela Cidade.

---
+++ src/modulo-08-java/aula4/src/main/java/br/com/crescer/aula4/mapeamentoBaseNunes/Cidade.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula4.mapeamentoBaseNunes;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Diuly
+ */
+@Entity
+@Table(name = "Cidade")
+public class Cidade implements Serializable {
+
+    @Id // Identifica a PK
+    @GeneratedValue(strategy = SEQUENCE, generator = "SQCIDADE")
+    @SequenceGenerator(name = "SQCIDADE", sequenceName = "SQCIDADE", allocationSize = 1)
+    @Basic(optional = false)
+    @Column(name = "IDCIDADE")
+    private Long idCidade;
+
+    @Basic(optional = false)
+    @Column(name = "NOME")
+    private String nomeCidade;
+
+    @Basic(optional = false)
+    @Column(name = "UF")
+    private String uf;
+
+    public Long getIdCidade() {
+        return idCidade;
+    }
+
+    public void setIdCidade(Long idCidade) {
+        this.idCidade = idCidade;
+    }
+
+    public String getNomeCidade() {
+        return nomeCidade;
+    }
+
+    public void setNomeCidade(String nomeCidade) {
+        this.nomeCidade = nomeCidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
